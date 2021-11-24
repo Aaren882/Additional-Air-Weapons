4bc2fc9c74f128a31b3c7eb0673e5718f85b423c
Aaren <aaren.yang@gmail.com>
2021-11-24 21:35:31 +0800

    Main Patch

---
+++ config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches
+{
+	class ADtional_WP
+	{
+		units[]={};
+		weapons[]={};
+		requiredAddons[]=
+		{
+			"A3_Weapons_F",
+			"A3_Weapons_F_Orange",
+			"A3_Data_F_ParticleEffects",
+			"ADtionBms",
+			"ADtionRks",
+			"ADtionRksMn",
+			"ADtionRksArty"
+		};
+		requiredVersion=0.1;
+	};
+};
+#include "Sounds\Sounds.hpp"
+#include "Curator\Supports.hpp"
+#include "Effects.hpp"
